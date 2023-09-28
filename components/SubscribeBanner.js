6cc866a4c7bb902ca6512e55fcd43539f5f307cb
mustufa100 <57327694+mustufa100@users.noreply.github.com>
2023-09-28 17:35:39 +0500

    changes

--- components/SubscribeBanner.js
+++ components/SubscribeBanner.js
@@ -1,37 +1,55 @@
 import React from "react"
 import { TitleLogo } from "./common/Title"
 import Link from "next/link"
-import { useRef } from "react"
+import { useRef,useState } from "react"
 import emailjs from "@emailjs/browser"
 
 const SubscribeBanner = () => {
 
-    const form = useRef()
-    const sendEmail = (e) => {
-        e.preventDefault();
-    
-        emailjs.sendForm('service_3kp1yga', 'template_gk1hgxi', form.current, '6q7UUQg6Qj5fz_hq-')
-          .then((result) => {
-              console.log(result.text);
-              form.current.reset();
+  const form = useRef();
+  const [emailError, setEmailError] = useState("");
 
-          }, (error) => {
-              console.log(error.text);
-          });
-      };
+  const validateEmail = (email) => {
+    // Basic email format validation
+    const regex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    return regex.test(email);
+  };
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const userEmail = form.current.querySelector("input[type='email']").value;
+
+    if (!validateEmail(userEmail)) {
+      setEmailError("Please provide a valid email address.");
+      return;
+    }
+
+    // Reset the email error
+    setEmailError("");
+
+    emailjs
+      .sendForm("service_ei51hur", "template_6lb5v9w", form.current, "6q7UUQg6Qj5fz_hq-")
+      .then((result) => {
+        console.log(result.text);
+        form.current.reset();
+      })
+      .catch((error) => {
+        console.log(error.text);
+      });
+  };
 
   return (
     <>
       <section className='banner'>
-      <form ref={form} className='email-container' onSubmit={sendEmail}>
+      <form ref={form} onSubmit={sendEmail}>
 
         <div className='container'>
           <div>
             <TitleLogo title='Subscribe To Our NewsLetter' />
             <br />
-            <input type="text" className="subscribe-input" placeholder="Enter your email" name="user_email" />
-            
+            <input type="email" className="subscribe-input" placeholder="Enter your email" name="user_email"/>
+            {emailError && <div className="error2">{emailError}</div>}
+
           </div>
           <div className="links">
             <button className='button-primary'>Subscribe</button>
--- sections/Contact.js
+++ sections/Contact.js
@@ -1,139 +1,209 @@
-import { Title, TitleSm } from "@/components/common/Title"
-import Link from "next/link"
-import React,{ useRef } from "react"
-import {  AiFillGoogleSquare, AiFillLinkedin } from "react-icons/ai"
-import { BiUserCircle } from "react-icons/bi"
-import { BsFacebook, BsTwitter } from "react-icons/bs"
-import { FiHeadphones, FiHelpCircle } from "react-icons/fi"
-import { IoLocationOutline } from "react-icons/io5"
-import { TbBrandWhatsapp,TbBrandUpwork } from "react-icons/tb";
-import emailjs from "@emailjs/browser"
+import { Title, TitleSm } from "@/components/common/Title";
+import Link from "next/link";
+import React, { useRef, useState } from "react";
+import {
+  AiFillGoogleSquare,
+  AiFillLinkedin,
+  AiFillFacebook,
+} from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
+import { BsTwitter } from "react-icons/bs";
+import { FiHeadphones, FiHelpCircle } from "react-icons/fi";
+import { IoLocationOutline } from "react-icons/io5";
+import { TbBrandWhatsapp, TbBrandUpwork } from "react-icons/tb";
+import emailjs from "@emailjs/browser";
+import "react-phone-input-2/lib/style.css"; // Import the styles
 
 const Contact = () => {
+  const form = useRef();
+  const [emailError, setEmailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
 
-  const form = useRef()
-    const sendEmail = (e) => {
-        e.preventDefault();
-    
-        emailjs.sendForm('service_i9e6vop', 'template_4sgzgyo', form.current, '6q7UUQg6Qj5fz_hq-')
-          .then((result) => {
-              console.log(result.text);
-              form.current.reset();
-          }, (error) => {
-              console.log(error.text);
-          });
-      };
+  const validateEmail = (email) => {
+    // Basic email format validation
+    const regex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    return regex.test(email);
+  };
 
+  const isGmail = (email) => {
+    const domain = email.split("@")[1];
+    return domain === "gmail.com";
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    // Validate that the phone number is not empty
+    if (!phoneNumber) {
+      setPhoneError("Phone number is required.");
+      return false;
+    }
+
+    // You can add more sophisticated phone number validation here if needed
+
+    return true;
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const userEmail = form.current["user_email"].value;
+
+    if (!validateEmail(userEmail)) {
+      setEmailError("Please provide a valid email address.");
+      return;
+    }
+
+    if (!isGmail(userEmail)) {
+      setEmailError("Please use a Gmail email address.");
+      return;
+    }
+
+    const userPhoneNumber = form.current["user_number"].value;
+
+    if (!validatePhoneNumber(userPhoneNumber)) {
+      // Phone number validation failed, don't proceed with sending the email
+      return;
+    }
+
+    // Reset the email and phone number errors
+    setEmailError("");
+    setPhoneError("");
+
+    emailjs
+      .sendForm(
+        "service_i9e6vop",
+        "template_4sgzgyo",
+        form.current,
+        "6q7UUQg6Qj5fz_hq-"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <>
-      <section className='contact bg-top'>
-        <div className='container'>
-          <div className='heading-title'>
-            <TitleSm title='CONTACT' /> <br />
+      <section className="contact bg-top">
+        <div className="container">
+          <div className="heading-title">
+            <TitleSm title="CONTACT" /> <br />
             <br />
-            <Title title="Let's start right now!" className='title-bg' />
+            <Title title="Let's start right now!" className="title-bg" />
           </div>
-          <div className='content py flex1'>
-            <div className='left w-30'>
-              <div className='contact-deatils'>
-                <div className='box'>
-                  <FiHeadphones size={30} className='icons' />
-                  <a href="whatsapp://send?phone=+923333008450"><h3>+92-333-300-8450</h3></a>
+          <div className="content py flex1">
+            <div className="left w-30">
+              <div className="contact-deatils">
+                <div className="box">
+                  <FiHeadphones size={30} className="icons" />
+                  <a href="whatsapp://send?phone=+923333008450">
+                    <h3>+92-333-300-8450</h3>
+                  </a>
                   <span>Call us: Mon - Fri 9:00 - 19:00</span>
                 </div>
-                <div className='box'>
-                  <IoLocationOutline size={30} className='icons' />
+                <div className="box">
+                  <IoLocationOutline size={30} className="icons" />
                   <h3>Karachi</h3>
                   <span></span>
                 </div>
-                <div className='box'>
-                  <FiHelpCircle size={30} className='icons' />
-                  <a href="mailto:kinetechsdigital@gmail.com"><h3>kinetechsdigital@gmail.com</h3></a>
+                <div className="box">
+                  <FiHelpCircle size={30} className="icons" />
+                  <a href="mailto:kinetechsdigital@gmail.com">
+                    <h3>kinetechsdigital@gmail.com</h3>
+                  </a>
                   <span>Drop us a line anytime!</span>
                 </div>
-                <div className='box'>
-                  <BiUserCircle size={30} className='icons' />
-                  <a href="mailto:kinetechsdigital@gmail.com"><h3>kinetechsdigital@gmail.com</h3></a>
+                <div className="box">
+                  <BiUserCircle size={30} className="icons" />
+                  <a href="mailto:kinetechsdigital@gmail.com">
+                    <h3>kinetechsdigital@gmail.com</h3>
+                  </a>
                   <span>Career at KINETECHS DIGITAL</span>
                 </div>
               </div>
               <ul>
-
                 <Link href="">
-                <li className='icon'>
-                  <BsTwitter size={25} />
-                </li>
+                  <li className="icon">
+                    <BsTwitter size={25} />
+                  </li>
                 </Link>
 
-
                 <Link href="">
-                <li className='icon'>
-                  <TbBrandUpwork size={25} />
-                </li>
+                  <li className="icon">
+                    <TbBrandUpwork size={25} />
+                  </li>
                 </Link>
 
-
                 <Link href="">
-                <li className='icon'>
-                  <AiFillGoogleSquare size={25} />
-                </li>
+                  <li className="icon">
+                    <AiFillGoogleSquare size={25} />
+                  </li>
                 </Link>
 
-
                 <Link href="">
-                <li className='icon'>
-                  <AiFillLinkedin size={25} />
-                </li>
+                  <li className="icon">
+                    <AiFillLinkedin size={25} />
+                  </li>
                 </Link>
-
 
                 <Link href="whatsapp://send?phone=+923333008450">
-                <li className='icon'>
-                  <TbBrandWhatsapp size={25} />
-                </li>
+                  <li className="icon">
+                    <TbBrandWhatsapp size={25} />
+                  </li>
                 </Link>
-
-
               </ul>
             </div>
-            <div className='right w-70'>
-              <TitleSm title='Make an online enquiry' />
-              <p className='desc-p'>Got questions? Ideas? Fill out the form below to get our proposal. </p>
+            <div className="right w-70">
+              <TitleSm title="Make an online enquiry" />
+              <p className="desc-p">
+                Got questions? Ideas? Fill out the form below to get our
+                proposal.
+              </p>
 
-              <form ref={form} onSubmit={sendEmail}>
-                <div className='grid-2'>
-                  <div className='inputs'>
+              <form ref={form} onSubmit={sendEmail} autoComplete="off">
+                <div className="grid-2">
+                  <div className="inputs">
                     <span>Name</span>
-                    <input type='text' name="user_name"/>
+                    <input type="text" name="user_name" required />
                   </div>
-                  <div className='inputs'>
+                  <div className="inputs">
                     <span>Email</span>
-                    <input type='text' name="user_email" required/>
+                    <input type="text" name="user_email" required />
+                    {emailError && <div className="error">{emailError}</div>}
                   </div>
                 </div>
-                <div className='grid-2'>
-                  <div className='inputs'>
-                    <span>your budget</span>
-                    <input type='text' name="user_budget"/>
+                <div className="grid-2">
+                  <div className="inputs">
+                    <span>Your Budget</span>
+                    <input type="number" name="user_budget" required />
                   </div>
-                  <div className='inputs'>
-                    <span>time frame</span>
-                    <input type='text' name="user_timeframe"/>
+                  <div className="inputs">
+                    <span>Phone Number</span>
+                    <input
+                      type="text"
+                      name="user_number"
+                      required
+                      placeholder="E.g., +1234567890"
+                    />
+                    {phoneError && <div className="error">{phoneError}</div>}
                   </div>
                 </div>
-                <div className='inputs'>
+                <div className="inputs">
                   <span>TELL US A BIT ABOUT YOUR PROJECT*</span>
-                  <textarea cols='30' rows='10'name="message"></textarea>
+                  <textarea cols="30" rows="10" name="message"></textarea>
                 </div>
-                <button className='button-primary'>Submit</button>
+                <button className="button-primary">Submit</button>
               </form>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;

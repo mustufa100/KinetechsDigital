1ff224a8a335714a093df13512a6defd29e86e7f
mustufa100 <57327694+mustufa100@users.noreply.github.com>
2024-02-10 15:05:39 +0500

    Update TransitionEffect.js

--- components/TransitionEffect.js
+++ components/TransitionEffect.js
@@ -15,7 +15,7 @@ const TransitionEffect = () => {
         animate={{x:"0%",width:"0%"}}
         transition={{delay:0.2,duration:0.8,ease:"easeInOut"}}
         />
-         <motion.div className='black'
+         <motion.div className='bla'
         initial={{x:"100%",width:"100%"}}
         animate={{x:"0%",width:"0%"}}
         transition={{delay:0.4,duration:0.8,ease:"easeInOut"}}
